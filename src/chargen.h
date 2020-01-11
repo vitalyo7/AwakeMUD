c88d153f80eef2565b9a8852cddf3f8c120a2b96
Static Variable <vitalyo@gmail.com>
2020-01-11 14:30:53 -0500

    Update chargen.h

--- src/chargen.h
+++ src/chargen.h
@@ -1,17 +1,8 @@
-#ifndef __chargen_h__
-#define __chargen_h__
+#ifndef _chargen_h
+#define  _chargen_h
 
-#include "fsm.h"
+#define STARTING_LANGUAGE_SKILL_LEVEL 10
+#define STARTING_RESTRING_POINTS      5
 
-//extern const char *pc_race_types[];
-class Chargen
-{
-public:
-	Chargen();
-	~Chargen();
-	void create_parse(struct descriptor_data *d, const char *arg);
-private:
-	Fsm chargenFsm;
-};
-
+extern const char *pc_race_types[];
 #endif
